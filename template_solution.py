fe7bc379ba8868559b1bec75e71f566ea9ee6b1a
geekSponge <hxhbuaa@163.com>
2023-04-03 17:45:14 +0200

    try different methods

--- template_solution.py
+++ template_solution.py
@@ -3,6 +3,10 @@
 # First, we import necessary libraries:
 import numpy as np
 import pandas as pd
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.kernel_ridge import KernelRidge
+# Polynomial
+from sklearn.gaussian_process.kernels import DotProduct, RBF, Matern, RationalQuadratic
 
 def data_loading():
     """
@@ -36,9 +40,17 @@ def data_loading():
     X_train = np.zeros_like(train_df.drop(['price_CHF'],axis=1))
     y_train = np.zeros_like(train_df['price_CHF'])
     X_test = np.zeros_like(test_df)
-
+    
     # TODO: Perform data preprocessing, imputation and extract X_train, y_train and X_test
-
+    countries = train_df.columns.values.tolist()
+    for counNum, counName in enumerate(countries[1:]):
+        if counName == 'price_CHF':
+            y_train = train_df[counName].fillna(train_df[counName].mean())  
+        else:
+            X_train[:,counNum] = train_df[counName].fillna(train_df[counName].mean())  
+            X_test[:,counNum] = test_df[counName].fillna(test_df[counName].mean())  
+    col = 'price_CHF'
+    train_df[col] = train_df[col].fillna(train_df[col].mean()) 
     assert (X_train.shape[1] == X_test.shape[1]) and (X_train.shape[0] == y_train.shape[0]) and (X_test.shape[0] == 100), "Invalid data shape"
     return X_train, y_train, X_test
 
@@ -59,7 +71,24 @@ def modeling_and_prediction(X_train, y_train, X_test):
 
     y_pred=np.zeros(X_test.shape[0])
     #TODO: Define the model and fit it using training data. Then, use test data to make predictions
+    gpr = GaussianProcessRegressor(kernel=DotProduct())
+    gpr = GaussianProcessRegressor(kernel=RBF())
+    gpr = GaussianProcessRegressor(kernel=Matern())
+    gpr = GaussianProcessRegressor(kernel=RationalQuadratic())
 
+    gpr.fit(X_train, y_train)
+    
+    
+    y_pred = gpr.predict(X_test)
+    
+    
+    # kr = KernelRidge(alpha=1.0, kernel='rbf', gamma=0.1)
+    # kr.fit(X_train, y_train)
+    # y_pred = kr.predict(X_test)
+
+    
+    
+    
     assert y_pred.shape == (100,), "Invalid data shape"
     return y_pred
 
